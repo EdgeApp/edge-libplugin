8dc5a6f955bf5d551cc81294f78956438f2352d9
ahujsak <allen@missionvi.com>
2019-01-11 12:32:47 -0500

    completely remove callbacks experiment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import * as callbacks from './postCallback.js'
 import * as config from './config.js'
 import * as core from './core.js'
 import * as ui from './ui.js'
 import * as utils from './utils.js'
-export { core, config, ui, utils, callbacks }
+export { core, config, ui, utils }
--- src/postCallbacks.js
+++
@@ -1,5 +0,0 @@
-import { postWrapper } from './bridge.js'
-
-export function edgeCallback (data) {
-  return postWrapper({func: 'edgeCallback', data})
-}
